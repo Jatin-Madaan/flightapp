a9186e70da5459e011a12865dae883f2a5167dbb
prithvekumar <prithve01234@gmail.com>
2020-08-10 23:31:44 +0530

    conflict resolved

--- src/main/java/com/flightapp/controller/PaymentAndInvoiceController.java
+++ src/main/java/com/flightapp/controller/PaymentAndInvoiceController.java
@@ -1,4 +1,4 @@
-package com.flightapp;
+package com.flightapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
