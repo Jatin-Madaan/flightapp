6ef04872f6a7c8033712f51e0cf560c1f137dc5f
prithvekumar <prithve01234@gmail.com>
2020-08-11 08:42:13 +0530

    logger

--- src/main/java/com/flightapp/controller/PaymentAndInvoiceController.java
+++ src/main/java/com/flightapp/controller/PaymentAndInvoiceController.java
@@ -1,10 +1,16 @@
 package com.flightapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flightapp.entities.Booking;
@@ -14,18 +20,24 @@ import com.flightapp.service.IPaymentAndInvoiceService;
 @RestController
 @CrossOrigin(origins = "*")
 public class PaymentAndInvoiceController {
-	
+
 	@Autowired
 	IPaymentAndInvoiceService paymentAndInvoiceService;
 	
+	/*
+	 * Get bookingid from INVOICE page and return the booking details.
+	*/
 	@GetMapping(path="/getbookingbyid/{bookingid}")
-	public Booking getbookingbyid(@PathVariable int bookingid) {
+	public Booking getbookingbyid(@PathVariable int bookingid) throws Exception{
 		return paymentAndInvoiceService.getbookingbyid(bookingid);
 		
 	}
 	
+	/*
+	 * Get bookingid,userid,status,amount from PAYMENT page and return an integer.
+	*/
 	@GetMapping(path="/setbookingstatusbyid/{bookingid}/{userid}/{status}/{amount}")
-	public int setbookingstatusbyid(@PathVariable int bookingid,@PathVariable int userid,@PathVariable String status,@PathVariable long amount) {
+	public int setbookingstatusbyid(@PathVariable int bookingid,@PathVariable int userid,@PathVariable String status,@PathVariable long amount) throws Exception {
 		return paymentAndInvoiceService.setbookingstatusbyid(bookingid, userid, status, amount);
 	}
 
