db3de42b161ae856e2a5588680a8dd74cd1bc996
Maneesh Kumar <37939177+Maani09k@users.noreply.github.com>
2020-08-19 09:56:59 +0530

    added scheduleFlightId

--- src/main/java/com/flightapp/controller/BookingController.java
+++ src/main/java/com/flightapp/controller/BookingController.java
@@ -1,13 +1,18 @@
 package com.flightapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flightapp.entities.Passenger;
+import com.flightapp.entities.ScheduleFlight;
 import com.flightapp.service.IBookingService;
 
 @RestController
@@ -29,5 +34,12 @@ public class BookingController
 		{
 			throw new Exception(" Booking Already Exists!");
 		}
+	}
+	
+	@GetMapping("/getFlightById/{scheduleFlightId}")
+	public ScheduleFlight getScheduleFlightById(@PathVariable int scheduleFlightId)
+	{
+		
+		return bookingService.getScheduleFlightById(scheduleFlightId);
 	}
 }
--- src/main/java/com/flightapp/service/IBookingService.java
+++ src/main/java/com/flightapp/service/IBookingService.java
@@ -1,8 +1,12 @@
 package com.flightapp.service;
+import java.util.List;
+
 import com.flightapp.entities.Passenger;
+import com.flightapp.entities.ScheduleFlight;
 import com.flightapp.exception.BookingException;
 
 public interface IBookingService 
 {
 	public Passenger addPassenger(Passenger passenger) throws BookingException;
+	public ScheduleFlight getScheduleFlightById(int scheduleFlightId);
 }
